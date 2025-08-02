935209740aaffa5503e72b90a1e15f508cb5d2c3
Ritika-25 <ritika25dhotre@gmail.com>
2025-08-02 14:33:34 +0530

    initial commit

---
+++ reverseArray.java
@@ -0,0 +1,19 @@
+public class reverseArray {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        int first=0;
+        int last=arr.length-1;
+        while(first<last){
+            int temp=arr[first];
+            arr[first]=arr[last];
+            arr[last]=temp;
+            first++;
+            last--;
+        }
+        System.out.print("Reverse of arry is: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    
+    }
+}
